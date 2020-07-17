b8caa10c4db49c7c68820140608e81025fafced5
Matthew Rougely <matthewrougelysr@gmail.com>
2020-07-17 15:35:33 -0700

    hey

--- src/components/about-menu.js
+++ src/components/about-menu.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function AboutMenu(props) {
     return (
         <nav className="about-menu">
             <ul>
-                <li>Our mission</li>
-                <li>Our values</li>
-                <li>Our team</li>
-                <li>Our labradoodles</li>
+                <Link to="/about/mission">Our mission</Link>
+                <Link to="/about/values">Our values</Link>
+                <Link to="/about/team">Our team</Link>
+                <Link to="/about/labs">Our labradoodles</Link>
             </ul>
         </nav>
     );
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import AboutMenu from './about-menu';
+import { Route, Switch } from 'react-router-dom'
 
 export default function App(props) {
+
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
+            <Switch>
+            <Route path="/about" component={AboutMenu} /> 
+            </Switch>
         </div>
     );
 }
